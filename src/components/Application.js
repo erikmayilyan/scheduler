b3eec67d0141b2467f30d858f6111c9c37430b7d
Mrdo Mrdo <mayilyane9@gmail.com>
2021-09-29 14:36:31 -0700

    Commit all changes

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,11 +1,40 @@
-import React from "react";
-
+import DayList from "./DayList";
 import "components/Application.scss";
+import React, { useState } from "react";
 
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
+        <img className="sidebar--centered" src="images/logo.png" />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList days={days} day={day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
